215e5c82b4d70bfb9b3cd6f0b75aba193203fe9c
Usman <usman5171338@gmail.com>
2025-08-16 19:57:58 +0500

    Predicting prompts

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Ecommerce-item-category-classification-using-TfidfVectorizer
 
 data available from : https://www.kaggle.com/datasets/saurabhshahane/ecommerce-text-classification 
+
+
+
+![alt text](image.png)
+
+![alt text](image-1.png)
+
--- project.py
+++ project.py
@@ -33,4 +33,13 @@ pipe=Pipeline(
 )
 pipe.fit(X_train, y_train)  
 y_pred=pipe.predict(X_test)
-print(classification_report(y_test, y_pred))
+#print(classification_report(y_test, y_pred))
+
+test=["Comprehensive Python programming guide for data analysis and machine learning, 3rd edition.",
+      "Men’s slim-fit cotton t-shirt, crew neck, breathable fabric, size M.",
+      "Latest smartphone with 128GB storage, 6GB RAM, 48MP camera, and fast charging."]
+prediction = pipe.predict(test)
+categories = {0: 'Household', 1: 'Books', 2: 'Electronics', 3: 'Clothing & Accessories'}
+
+for desc, pred in zip(test, prediction):
+    print(f"Description: {desc}\nPredicted Category: {categories[pred]}\n")
